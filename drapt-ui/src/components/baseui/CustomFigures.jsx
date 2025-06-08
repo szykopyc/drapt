1fe33241d00028549d1a5fc1a5b8149288ffe7c1
Szymon Kopycinski <szymonkopy@gmail.com>
2025-06-09 00:30:23 +0100

    Added loading="lazy" to images

--- drapt-ui/src/components/baseui/CustomFigures.jsx
+++ drapt-ui/src/components/baseui/CustomFigures.jsx
@@ -1,7 +1,7 @@
 export function FigureCap({srcfile, alt, children}){
     return (
         <figure className='flex flex-col items-center my-2'>
-            <img src={srcfile} alt={alt} className='w-full sm:w-2/3 md:w-1/2 rounded' />
+            <img src={srcfile} alt={alt} className='w-full sm:w-2/3 md:w-1/2 rounded' loading="lazy"/>
             <figcaption className='text-xs italic text-base-content/60 mt-1 text-center'>
                 {children}
             </figcaption>
@@ -12,7 +12,7 @@ export function FigureCap({srcfile, alt, children}){
 export function SmallerFigureCap({srcfile, alt, children}){
     return (
         <figure className='flex flex-col items-center w-full sm:w-3/4 md:w-1/2 lg:w-2/5'>
-                <img src={srcfile} alt={alt} className='w-full rounded' />
+                <img src={srcfile} alt={alt} className='w-full rounded' loading="lazy"/>
                 <figcaption className='text-xs italic text-white mt-1 text-center'>
                     {children}
                 </figcaption>
