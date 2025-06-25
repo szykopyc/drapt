0b775df4c1ca9e5fc077da3d76e1cf55ec47b6e9
Szymon Kopycinski <szymonkopy@gmail.com>
2025-06-26 00:45:58 +0200

    Updated landing page, other UI stuff
    Finally worked a bit on analyse index page, removed redundant select portfolio from performance/risk panel. Changed GlobalAnalysisCard, and probably some other stuff. Oh yeah also changed the login card

--- drapt-ui/src/pages/ForgotPassword.jsx
+++ drapt-ui/src/pages/ForgotPassword.jsx
@@ -1,33 +1,43 @@
-import { Link } from 'react-router-dom';
-import { MainBlock } from '../components/baseui/MainBlock';
-import { BeginText } from '../components/baseui/BeginText';
-import { CardOne } from '../components/baseui/CustomCard';
-import { useRef, useState } from 'react';
-import { useForm } from 'react-hook-form';
-import { ModalHelper } from '../components/helperui/ModalHelper';
-import { isValidEmail } from '../components/validators/EmailValidator';
-import { getPasswordStrength } from '../components/validators/PasswordValidator';
-import LargeSubmit from '../components/baseui/LargeSubmitHelper';
-import { FormField } from '../components/helperui/FormFieldHelper';
+import { Link } from "react-router-dom";
+import { MainBlock } from "../components/baseui/MainBlock";
+import { BeginText } from "../components/baseui/BeginText";
+import { CardOne } from "../components/baseui/CustomCard";
+import { useRef, useState } from "react";
+import { useForm } from "react-hook-form";
+import { ModalHelper } from "../components/helperui/ModalHelper";
+import { isValidEmail } from "../components/validators/EmailValidator";
+import { getPasswordStrength } from "../components/validators/PasswordValidator";
+import LargeSubmit from "../components/baseui/LargeSubmitHelper";
+import { FormField } from "../components/helperui/FormFieldHelper";
 
-export default function ForgotPassword(){
+export default function ForgotPassword() {
     const userResetPasswordModalRef = useRef(null);
     const [modalData, setModalData] = useState(null);
 
-    const {register, handleSubmit, reset: resetForm, watch, formState: {errors} } = useForm({
-        mode:"onChange"
-    })
+    const {
+        register,
+        handleSubmit,
+        reset: resetForm,
+        watch,
+        formState: { errors },
+    } = useForm({
+        mode: "onChange",
+    });
 
     const onSubmit = (data) => {
-        if (!isValidEmail(data.email) && (getPasswordStrength(data.password)=="Weak")){
+        if (
+            !isValidEmail(data.email) &&
+            getPasswordStrength(data.password) == "Weak"
+        ) {
             return;
         }
-        
+
         // API call to send email will be here. For now, only the modal will be shown...
         setModalData(data);
-        if (userResetPasswordModalRef.current) userResetPasswordModalRef.current.showModal();
+        if (userResetPasswordModalRef.current)
+            userResetPasswordModalRef.current.showModal();
         resetForm();
-    }
+    };
 
     const typedEmail = watch("email");
     const emailValid = isValidEmail(typedEmail);
@@ -36,34 +46,59 @@ export default function ForgotPassword(){
     return (
         <MainBlock>
             <BeginText title={"Forgot your password?"}>
-                <p>Please follow the instructions below to reset your password.</p>
+                <p>
+                    Please follow the instructions below to reset your password.
+                </p>
             </BeginText>
-            <div className='divider my-0'></div>
+            <div className="divider my-0"></div>
             <CardOne id={"resetPasswordCard"} title={"Reset Password"}>
-                <form id="resetPassword" className='flex flex-col gap-3 w-full' onSubmit={handleSubmit(onSubmit)} autoComplete='off'>
-                    <FormField label={"Email"} error={errors.email && errors.email.message}>
-                        <input 
-                            type="text" 
-                            className='input input-bordered w-full'
-                            {...register("email", {required:"Email is required",
-                                validate: value => isValidEmail(value) || "Please enter a valid email address"
+                <form
+                    id="resetPassword"
+                    className="flex flex-col gap-3 w-full"
+                    onSubmit={handleSubmit(onSubmit)}
+                    autoComplete="off"
+                >
+                    <FormField
+                        label={"Email"}
+                        error={errors.email && errors.email.message}
+                    >
+                        <input
+                            type="text"
+                            className="input input-bordered w-full"
+                            {...register("email", {
+                                required: "Email is required",
+                                validate: (value) =>
+                                    isValidEmail(value) ||
+                                    "Please enter a valid email address",
                             })}
-                            autoComplete='off'
+                            autoComplete="off"
                         />
                     </FormField>
-                </form>    
+                </form>
                 <div className="flex flex-row gap-2 w-full">
-                    <LargeSubmit form={"resetPassword"} size={1} disabled={!fieldFilled}>
+                    <LargeSubmit
+                        form={"resetPassword"}
+                        size={1}
+                        disabled={!fieldFilled}
+                    >
                         Send password reset link
                     </LargeSubmit>
                 </div>
             </CardOne>
-            <ModalHelper id={"reset_password_modal"} reference={userResetPasswordModalRef} modalTitle={"Check your email"}>
+            <ModalHelper
+                id={"reset_password_modal"}
+                reference={userResetPasswordModalRef}
+                modalTitle={"Check your email"}
+            >
                 {modalData && (
                     <p>
-                    If an account exists for <span className='text-info'>{modalData.email}</span>, you'll receive an email with instructions to reset your password.
-                    <br />
-                    Please check your inbox and spam folder, and wait 5 minutes before attempting to reset your password again.
+                        If an account exists for{" "}
+                        <span className="text-info">{modalData.email}</span>,
+                        you'll receive an email with instructions to reset your
+                        password.
+                        <br />
+                        Please check your inbox and spam folder, and wait 5
+                        minutes before attempting to reset your password again.
                     </p>
                 )}
             </ModalHelper>
