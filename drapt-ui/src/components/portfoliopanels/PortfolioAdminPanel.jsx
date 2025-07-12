a083dd82b88cea67daf17413459442c97256aa80
Szymon Kopycinski <szymonkopy@gmail.com>
2025-07-12 13:50:51 +0100

    Portfolio deletion (dev only), portfolio admin panel integration of delete func

--- drapt-ui/src/components/portfoliopanels/PortfolioAdminPanel.jsx
+++ drapt-ui/src/components/portfoliopanels/PortfolioAdminPanel.jsx
@@ -4,16 +4,24 @@ import { MdErrorOutline } from "react-icons/md";
 import { LoadingSpinner } from "../helperui/LoadingSpinnerHelper";
 import { useHookSearchPortfolioOverview } from "../../reactqueryhooks/usePortfolioHook";
 
+import useUserStore from "../../stores/userStore";
+
 //toasts
 import { Toaster } from 'react-hot-toast';
 
 import AdminManageTeamCard from "../portfolioui/PortfolioAdminUI/AdminManageTeam";
 import AdminUpdatePortfolioDetails from "../portfolioui/PortfolioAdminUI/AdminUpdatePortfolioDetails";
+import AdminDeletePortfolio from "../portfolioui/PortfolioAdminUI/AdminDeletePortfolio";
 
 export function PortfolioAdminPanel() {
+
+  const user = useUserStore((state) => state.user);
+
   const { portfolioID } = useParams();
 
   const { isLoading, isError, error } = useHookSearchPortfolioOverview(portfolioID);
+
+  if (!user) return null;
 
   return (
     <>
@@ -31,6 +39,9 @@ export function PortfolioAdminPanel() {
         <>
           <AdminUpdatePortfolioDetails />
           <AdminManageTeamCard />
+          {["developer"].includes(user?.role) ? (
+            <AdminDeletePortfolio />
+          ) : null}
         </>
       )}
       <Toaster />
