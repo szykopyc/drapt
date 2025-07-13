818b54cd9d7969553cce39feed0fa109002ec891
Szymon Kopycinski <szymonkopy@gmail.com>
2025-07-13 17:31:45 +0100

    Checkpoint commit (no new features)

--- drapt-ui/src/pages/Landing.jsx
+++ drapt-ui/src/pages/Landing.jsx
@@ -15,149 +15,149 @@ import { checkAuth } from "../lib/AuthService";
 import { useNavigate } from "react-router-dom";
 
 export default function Landing() {
-    const user = useUserStore((state) => state.user);
-    const setUser = useUserStore((state) => state.setUser);
-    const setSessionExpired = useUserStore((state) => state.setSessionExpired);
-    const navigate = useNavigate();
-    if (!user) return null;
+  const user = useUserStore((state) => state.user);
+  const setUser = useUserStore((state) => state.setUser);
+  const setSessionExpired = useUserStore((state) => state.setSessionExpired);
+  const navigate = useNavigate();
 
-    const dummyPerformanceToRender = dummyPerformance;
+  const dummyPerformanceToRender = dummyPerformance;
 
-    const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(false);
 
-    const metricsRef = useRef(null);
-    const [metricsHeight, setMetricsHeight] = useState(0);
+  const metricsRef = useRef(null);
+  const [metricsHeight, setMetricsHeight] = useState(0);
 
-    useEffect(() => {
-        const authCheck = async () => {
-            try {
-                const response = await checkAuth();
-                if (response) {
-                    setUser(response);
-                    setSessionExpired(false);
-                }
-            } catch {
-                if (user) {
-                    setSessionExpired(true);
-                    navigate("/session-expired", { replace: true });
-                } else {
-                    setUser(null);
-                    navigate("/unauthorised", { replace: true });
-                }
-            }
-        };
-        authCheck();
-    }, []);
-
-    try {
-        var theme = localStorage.getItem("theme");
-        if (theme) {
-            document.documentElement.setAttribute("data-theme", theme);
+  useEffect(() => {
+    const authCheck = async () => {
+      try {
+        const response = await checkAuth();
+        if (response) {
+          setUser(response);
+          setSessionExpired(false);
         }
-    } catch (e) {}
+      } catch {
+        if (user) {
+          setSessionExpired(true);
+          navigate("/session-expired", { replace: true });
+        } else {
+          setUser(null);
+          navigate("/unauthorised", { replace: true });
+        }
+      }
+    };
+    authCheck();
+  }, []);
 
-    useEffect(() => {
-        setLoading(true);
-        setTimeout(() => {
-            setLoading(false);
-            if (metricsRef.current) {
-                setMetricsHeight(metricsRef.current.clientHeight);
-            }
-        }, 1000);
-    }, []);
+  try {
+    var theme = localStorage.getItem("theme");
+    if (theme) {
+      document.documentElement.setAttribute("data-theme", theme);
+    }
+  } catch (e) { }
 
-    return (
-        <MainBlock>
-            <BeginText
-                title={
-                    user ? `Welcome back, ${user?.fullname}` : `Welcome back`
-                }
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+      if (metricsRef.current) {
+        setMetricsHeight(metricsRef.current.clientHeight);
+      }
+    }, 1000);
+  }, []);
+
+  if (!user) return null;
+
+  return (
+    <MainBlock>
+      <BeginText
+        title={
+          user ? `Welcome back, ${user?.fullname}` : `Welcome back`
+        }
+      >
+        <p>
+          Get a broad sense of how your portfolio is currently
+          performing, and view any news you may have missed.
+        </p>
+      </BeginText>
+      {!loading && (
+        <>
+          <div className="divider my-0"></div>
+          {dummyPerformanceToRender.length == 0 ? (
+            <CardOne
+              id={"portfolioOverview"}
+              title={"Portfolio Overview"}
             >
-                <p>
-                    Get a broad sense of how your portfolio is currently
-                    performing, and view any news you may have missed.
-                </p>
-            </BeginText>
-            {!loading && (
-                <>
-                    <div className="divider my-0"></div>
-                    {dummyPerformanceToRender.length == 0 ? (
-                        <CardOne
-                            id={"portfolioOverview"}
-                            title={"Portfolio Overview"}
-                        >
-                            <InnerEmptyState
-                                title="No portfolio created yet"
-                                message="Create a portfolio, or add holdings to see a performance overview"
-                            />
-                        </CardOne>
-                    ) : (
-                        <CardOne
-                            id={"portfolioOverview"}
-                            title={
-                                ["vd", "director", "developer"].includes(
-                                    user?.role
-                                )
-                                    ? "NEFSIF Index Performance"
-                                    : `${
-                                          user?.team.charAt(0).toUpperCase() +
-                                          user?.team.slice(1)
-                                      } Portfolio Performance`
-                            }
-                        >
-                            <ChartNoBorderCard
-                                data={dummyPerformanceToRender}
-                            />
-                            {["vd", "director", "developer"].includes(
-                                user?.role
-                            ) ? (
-                                <div className="flex md:flex-row justify-between gap-1 md:gap-3">
-                                    <CustomButton to="/portfolio">
-                                        Navigate to Fund Scope
-                                    </CustomButton>
-                                </div>
-                            ) : (
-                                <div className="flex md:flex-row justify-between gap-1 md:gap-3">
-                                    <CustomButton to="/analyse">
-                                        Analyse
-                                    </CustomButton>
-                                    <CustomButton to="/portfolio">
-                                        Portfolio
-                                    </CustomButton>
-                                </div>
-                            )}
-                        </CardOne>
-                    )}
-                    <CardHelper>
-                        <CardOne title={"Aggregate Portfolio Metrics"}>
-                            <div ref={metricsRef}>
-                                <CustomTable data={dummyPortfolioMetrics} />
-                            </div>
-                        </CardOne>
-                        <CardOne title={"News Catchup"}>
-                            <CustomTable
-                                data={dummyNews}
-                                maxHeight={
-                                    metricsHeight
-                                        ? `${metricsHeight}px`
-                                        : "389px"
-                                }
-                            />
-                        </CardOne>
-                    </CardHelper>
-                </>
-            )}
+              <InnerEmptyState
+                title="No portfolio created yet"
+                message="Create a portfolio, or add holdings to see a performance overview"
+              />
+            </CardOne>
+          ) : (
+            <CardOne
+              id={"portfolioOverview"}
+              title={
+                ["vd", "director", "developer"].includes(
+                  user?.role
+                )
+                  ? "NEFSIF Index Performance"
+                  : `${user?.team.charAt(0).toUpperCase() +
+                  user?.team.slice(1)
+                  } Portfolio Performance`
+              }
+            >
+              <ChartNoBorderCard
+                data={dummyPerformanceToRender}
+              />
+              {["vd", "director", "developer"].includes(
+                user?.role
+              ) ? (
+                <div className="flex md:flex-row justify-between gap-1 md:gap-3">
+                  <CustomButton to="/portfolio">
+                    Navigate to Fund Scope
+                  </CustomButton>
+                </div>
+              ) : (
+                <div className="flex md:flex-row justify-between gap-1 md:gap-3">
+                  <CustomButton to="/analyse">
+                    Analyse
+                  </CustomButton>
+                  <CustomButton to="/portfolio">
+                    Portfolio
+                  </CustomButton>
+                </div>
+              )}
+            </CardOne>
+          )}
+          <CardHelper>
+            <CardOne title={"Aggregate Portfolio Metrics"}>
+              <div ref={metricsRef}>
+                <CustomTable data={dummyPortfolioMetrics} />
+              </div>
+            </CardOne>
+            <CardOne title={"News Catchup"}>
+              <CustomTable
+                data={dummyNews}
+                maxHeight={
+                  metricsHeight
+                    ? `${metricsHeight}px`
+                    : "389px"
+                }
+              />
+            </CardOne>
+          </CardHelper>
+        </>
+      )}
 
-            {loading && (
-                <>
-                    <div className="divider my-0"></div>
-                    <div className="skeleton w-full h-[542px]"></div>
-                    <CardHelper>
-                        <div className="skeleton w-full h-[477px]"></div>
-                        <div className="skeleton w-full h-[477px]"></div>
-                    </CardHelper>
-                </>
-            )}
-        </MainBlock>
-    );
+      {loading && (
+        <>
+          <div className="divider my-0"></div>
+          <div className="skeleton w-full h-[542px]"></div>
+          <CardHelper>
+            <div className="skeleton w-full h-[477px]"></div>
+            <div className="skeleton w-full h-[477px]"></div>
+          </CardHelper>
+        </>
+      )}
+    </MainBlock>
+  );
 }
